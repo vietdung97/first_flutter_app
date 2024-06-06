f1f718a38021a8ffd2319b784ac8cdc9e228d8e5
vietdung97 <ngovietdung0112@gmail.com>
2024-06-07 00:15:03 +0700

    Render conditional UI, lifting state

---
+++ lib/quiz.dart
@@ -0,0 +1,45 @@
+import 'package:first_app/questions_screen.dart';
+import 'package:first_app/start_screen.dart';
+import 'package:flutter/material.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() {
+    return _QuizState();
+  }
+}
+
+class _QuizState extends State<Quiz> {
+  Widget? activeScreen;
+
+  @override
+  void initState() {
+    activeScreen = StartScreen(switchScreen);
+    super.initState();
+  }
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = const QuestionsScreen();
+    });
+  }
+
+  @override
+  Widget build(context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      color: Colors.transparent,
+      home: Scaffold(
+        body: Container(
+            decoration: const BoxDecoration(
+                gradient: LinearGradient(colors: [
+              Colors.deepPurple,
+              Color.fromARGB(255, 202, 63, 227),
+            ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
+            child: activeScreen),
+      ),
+    );
+  }
+}
--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 class StartScreen extends StatelessWidget {
-  const StartScreen({super.key});
+  const StartScreen(this.startQuiz, {super.key});
+
+  final void Function() startQuiz;
 
   @override
   Widget build(context) {
@@ -9,9 +11,17 @@ class StartScreen extends StatelessWidget {
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
+          // Opacity(
+          //   opacity: 0.5,
+          //   child: Image.asset(
+          //     'assets/images/quiz-logo.png',
+          //     width: 300,
+          //   ),
+          // ),
           Image.asset(
             'assets/images/quiz-logo.png',
             width: 300,
+            color: const Color.fromARGB(57, 255, 255, 255),
           ),
           const SizedBox(
             height: 80,
@@ -23,11 +33,11 @@ class StartScreen extends StatelessWidget {
           const SizedBox(
             height: 30,
           ),
-          OutlinedButton(
-              onPressed: () {},
-              style:
-                  OutlinedButton.styleFrom(foregroundColor: Colors.white),
-              child: const Text(
+          OutlinedButton.icon(
+              onPressed: startQuiz,
+              icon: const Icon(Icons.arrow_right_alt),
+              style: OutlinedButton.styleFrom(foregroundColor: Colors.white),
+              label: const Text(
                 'Start Quiz',
                 style: TextStyle(color: Colors.white, fontSize: 20),
               )),
